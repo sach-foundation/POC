69d2ca7240a3024195814dfd90e45cb39f118700
usman <mohd.usman@india.com>
2023-08-29 17:25:46 +0530

    Viper added

---
+++ db/util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// Locad the file or override if required
+func LoadConfig(path string) (config Config, err error) {
+
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env") //json  xml can also be used
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"GOLANG/.vscode/api"
 	db "GOLANG/db/sqlc"
+	"GOLANG/db/util"
 	"database/sql"
 	_ "database/sql"
 	"log"
@@ -10,15 +11,21 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "localhost:9000"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "localhost:9000"
+// )
 
 func main() {
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("Unable to load config file", err)
+	}
+	// conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to DB", err)
@@ -27,7 +34,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("Cannot start the server", err.Error())
