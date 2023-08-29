26237f10b694013f736c6d5edfa8232445c0664a
usman <mohd.usman@india.com>
2023-08-29 17:58:11 +0530

    Viper added for Test and API

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"GOLANG/db/util"
 	"database/sql"
 	"log"
 	"os"
@@ -9,17 +10,24 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
+// const (
+// 	dbDriver = "postgres"
+// 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// )
 
 var testQueries *Queries
 var testDb *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDb, err = sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot connect to DB", err)
+	}
+
+	// var err error
+	// testDb, err = sql.Open(dbDriver, dbSource)
+	testDb, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to DB", err)
